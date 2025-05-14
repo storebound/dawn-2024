972aabbf8615c42c2021810503cd85a93a45796e
JulianSchnehage <julian@dev-a-development.com>
2025-05-14 22:27:04 +0200

    update javascript file

--- assets/is_custom.js
+++ assets/is_custom.js
@@ -1,9 +1,11 @@
-const ON_CHANGE_DEBOUNCE_TIMER = 300;
+/** Tooltips for PDP and PLP product cards that have compare at prices.
+* Also called on product-info.js and facets.js and related-products.liquid
+*/
+document.addEventListener('DOMContentLoaded', function () {
+  const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+});
 
-const PUB_SUB_EVENTS = {
-  cartUpdate: 'cart-update',
-  quantityUpdate: 'quantity-update',
-  optionValueSelectionChange: 'option-value-selection-change',
-  variantChange: 'variant-change',
-  cartError: 'cart-error',
-};
+
