181414005a993b6a8a03edffb28e52e688afff6e
Jason Tan <jasonzhetan@yahoo.com>
2025-11-12 12:33:41 -0500

    more role

--- assets/accessibility.js
+++ assets/accessibility.js
@@ -2,8 +2,15 @@ new MutationObserver(() => {
   document.querySelectorAll('picture').forEach(el => {
     el.removeAttribute('role')
   })
+  document.querySelectorAll('div[role="button"]').forEach(el => {
+    el.removeAttribute('role')
+  })
 }).observe(document.body, { childList: true, subtree: true });
 
 document.querySelectorAll('picture').forEach(el => {
+  el.removeAttribute('role')
+})
+
+document.querySelectorAll('div[role="button"]').forEach(el => {
   el.removeAttribute('role')
 })
