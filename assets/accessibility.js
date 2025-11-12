851829f41a66b836cbbe2dc0ccd3f528f7919ba6
Jason Tan <jasonzhetan@yahoo.com>
2025-11-12 14:09:24 -0500

    more of the script

--- assets/accessibility.js
+++ assets/accessibility.js
@@ -1,10 +1,20 @@
 new MutationObserver(() => {
   document.querySelectorAll('picture').forEach(el => {
     el.removeAttribute('role')
-  })
+  });
   document.querySelectorAll('div[role="button"]').forEach(el => {
     el.removeAttribute('role')
-  })
+  });
+  document.querySelectorAll('img').forEach(el => {
+    if (!el.hasAttribute('alt')) {
+        el.setAttribute('alt', '')
+    }
+  });
+  document.querySelectorAll('button[data-replo-add-product-variant-to-cart]').forEach(el => {
+    if (!el.hasAttribute('aria-label')) {
+        el.setAttribute('aria-label', 'Add to cart')
+    }
+  });
 }).observe(document.body, { childList: true, subtree: true });
 
 document.querySelectorAll('picture').forEach(el => {
@@ -13,4 +23,16 @@ document.querySelectorAll('picture').forEach(el => {
 
 document.querySelectorAll('div[role="button"]').forEach(el => {
   el.removeAttribute('role')
+})
+
+document.querySelectorAll('img').forEach(el => {
+    if (!el.hasAttribute('alt')) {
+        el.setAttribute('alt', '')
+    }
+})
+
+document.querySelectorAll('button[data-replo-add-product-variant-to-cart]').forEach(el => {
+    if (!el.hasAttribute('aria-label')) {
+        el.setAttribute('aria-label', 'Add to cart')
+    }
 })
